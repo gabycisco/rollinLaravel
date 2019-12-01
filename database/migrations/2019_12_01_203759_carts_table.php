95c910a0768814ffc8a5b67b779e8bfe6eb7c35a
Marina Ziembar <marinaziembar@gmail.com>
2019-12-01 17:58:15 -0300

    Agrego migrations
    Tabla de carrito y tabla relacional carrito_producto. No se olviden de hacer migrate.

---
+++ database/migrations/2019_12_01_203759_carts_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CartsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('charts', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('user_id',);
+            $table->char('status',50);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts');
+    }
+}
---
+++ database/migrations/2019_12_01_204848_cart_product_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CartProductTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cart_product', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->bigInteger('cart_id',);
+            $table->bigInteger('product_id');
+            $table->integer('quantity');
+            $table->decimal('price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cart_product');
+    }
+}
