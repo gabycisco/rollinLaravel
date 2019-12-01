5d14cb8df11cd8242113e5a9c9f80c061adeb95c
Marina Ziembar <marinaziembar@gmail.com>
2019-12-01 19:06:16 -0300

    view store
    Maquetado HTML de  la view del store.

--- resources/views/store.blade.php
+++ resources/views/store.blade.php
@@ -4,6 +4,12 @@
 @endsection
 @section("principal")
 
-
+<h1>TIENDA</h1>
+<div> <!--para cargar productos-->
+<a href="(dirije a producto)"><img src="(aca va la foto)" alt="fotoproducto"></a>
+<a href="(dirije a producto)">NOMBRE DEL PRODUCTO</a>
+<p>descripción de producto</p>
+<p>PRECIO</p>
+</div>
 
 @endsection
