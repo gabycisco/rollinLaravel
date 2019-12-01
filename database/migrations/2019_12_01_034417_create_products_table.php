b90bd23f2b3caa76b8ba4cc513ede86e9f136e53
Juan Martin Magallanes <juanmartinmagallanes@gmail.com>
2019-12-01 01:03:38 -0300

    Migracion Products y Brands

---
+++ database/migrations/2019_12_01_034417_create_products_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string("name");
+            $table->integer("precio");
+            $table->char("brand_id",100)->nullable();
+            $table->tinyInteger("active")->default(1);
+            $table->char("img",250)->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products');
+    }
+}
