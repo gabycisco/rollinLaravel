3aa808b2c31e2412932583e01ec784a7e9d8954d
Marina Ziembar <marinaziembar@gmail.com>
2019-12-15 16:50:58 -0300

    Validacion Newsletter
    Agrego validacion fulera.

--- app/Http/Controllers/SubscriberController.php
+++ app/Http/Controllers/SubscriberController.php
@@ -10,6 +10,19 @@ class SubscriberController extends Controller
     public function create (Request $formulario)
     {
 
+      
+      $reglas=[
+        'email'=>"e-mail|required|min:5", 
+
+      ];
+
+      $mensajes=[
+        'required'=> "Este campo es obligatorio",
+        'min'=> "Este campo requiere al menos 5 carateres",
+        'e-mail'=>"El formato email no es válido",
+      ];
+      
+      $this->validate($formulario, $reglas,$mensajes);
 
       $NewSubscriber = new Subscriber();
       $NewSubscriber->email = $formulario["email"];
